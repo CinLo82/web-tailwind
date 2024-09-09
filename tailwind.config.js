a8c503764a63a39b60e2fe8c87e56a566a288826
Cintia Losada <cinlos3382@gmail.com>
2024-09-09 16:51:10 -0300

    animaciones en el logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,24 @@ module.exports = {
             colors: {
                 'azul-claro': '#37bcf9',
                 'azul-oscuro': '#0370b9'
-            }
+            },
+            animation: {
+                'spin-slow': 'spin 2s linear infinite',
+                'from-bellow': 'fromBellow 500ms linear',
+                'from-right': 'fromRight 300ms linear',
+            },
+            keyframes: {
+                fromBellow: {
+                    '0%': { transform: 'translateY(0%)' },
+                    '50%': { transform: 'translateY(200%)' },
+                    '100%': { transform: 'translateY(0%)' },
+                },
+                fromRight: {
+                    '0%': { transform: 'translateX(200%)' },
+                    '100%': { transform: 'translateX(0%)' },
+                },
+            },
+
         },
     },
     plugins: [],
