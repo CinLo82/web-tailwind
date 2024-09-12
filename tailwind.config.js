ebb8ff1d29b04fb6e60f74aa20be7f47f2495e9b
Cintia Losada <cinlos3382@gmail.com>
2024-09-11 21:19:48 -0300

    maquetacion, estilado y responsive del banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
             },
             backgroundImage: {
                 'body-pattern': "url('../img/pattern.png')",
+                'banner': "url('../img/bakbaner.png')"
+            },
+            backgroundPosition: {
+                'banner-position': '-200px -200px'
             },
             colors: {
                 'azul-claro': '#37bcf9',
@@ -20,6 +24,8 @@ module.exports = {
                 'spin-slow': 'spin 2s linear infinite',
                 'from-bellow': 'fromBellow 500ms linear',
                 'from-right': 'fromRight 300ms linear',
+                'bg-banner': 'backBanner 10s linear',   
+                'text-banner': 'showBannerText 10s linear',
             },
             keyframes: {
                 fromBellow: {
@@ -31,8 +37,33 @@ module.exports = {
                     '0%': { transform: 'translateX(200%)' },
                     '100%': { transform: 'translateX(0%)' },
                 },
+                backBanner: {
+                    '0%': { backgroundPosition: '0% 0%' },
+                    '100%': { backgroundPosition: '-200% -200%' },
+                },
+                showBannerText: {
+                    '0%': { 
+                        transform: 'translateX(-600%) scale(5.5)',
+                        opacity: '1' 
+                    },
+                    '50%': { 
+                        transform: 'translateX(600%) scale(5.5)',
+                        opacity: '1' 
+                    },
+                    '75%': { 
+                        transform: 'translateX(-600%) scale(5.5)',
+                        'text-shadow': 'none',
+                        color: 'transparent',
+                        opacity: '0' 
+                    },
+                    '100%': { 
+                        transform: 'translateX(-600%) scale(5.5)',
+                        'text-shadow': '2px 2px 1px black',
+                        color: 'white',
+                        opacity: '1'  
+                    },
+                }
             },
-
         },
     },
     plugins: [],
